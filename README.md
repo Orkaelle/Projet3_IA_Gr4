d97b3684b5ccd7273be2034ec643396bbbc40a6e
Orkaëlle <orkaelle.hardelin@gmail.com>
2020-08-27 17:05:59 +0200

    Final push

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Projet3_IA_Gr4
+Projet 3 - Scandale ENRON
+Groupe 4 - Issam, Nicolas C, Orkaëlle
+
+Les résultats sont disponibles dans le dossier "Results" de la branche Master.
+
+1 - Création d'un échantillon : creation_echantillon.py
+2 - Parsing des mails de l'échantillon et sauvegarde du dataset en csv : parsing_emails.py
+3 - Exploitation des données : data_exploitation.py
+4 - Clustering : recherche.py (lien vers l'archive GoogleNews-vectors-negative300.bin.gz : https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit à télécharger et à extraire dans le même répertoire que le fichier recherche.py)
+
+
+
--- creation_echantillon.py
+++ creation_echantillon.py
@@ -4,43 +4,62 @@ import shutil
 import numpy
 
 
-def creation_echantillon_mails (nbEchantillon, tailleEchantillon) :
+def creation_echantillon_mails(nbEchantillon, tailleEchantillon):
     """Création d'un échantillon de mails parmis tous les mails récupérés dans le brief"""
-    print ('\nCréation de', nbEchantillon, 'échantillon(s) de', tailleEchantillon, 'mail(s)...\n')
+    print(
+        '\nCréation de',
+        nbEchantillon,
+        'échantillon(s) de',
+        tailleEchantillon,
+        'mail(s)...\n')
 
     # Définition des différents répertoires
     dirname = os.path.abspath(os.path.split(__file__)[0])
-    mailDir = os.path.join(dirname,'maildir')
+    mailDir = os.path.join(dirname, 'maildir')
 
     # Comptage des emails
-    mails_count = sum(len(files) for _, _, files in os.walk(mailDir)) 
+    mails_count = sum(len(files) for _, _, files in os.walk(mailDir))
 
     # On purge le dossier cible
-    if not os.path.exists(os.path.join(dirname,'echantillons')):
-        os.makedirs(os.path.join(dirname,'echantillons'))
-    else :
-        shutil.rmtree(os.path.join(dirname,'echantillons'))
+    if not os.path.exists(os.path.join(dirname, 'echantillons')):
+        os.makedirs(os.path.join(dirname, 'echantillons'))
+    else:
+        shutil.rmtree(os.path.join(dirname, 'echantillons'))
 
-    for echantillon in range(1, nbEchantillon+1) :
-        print ('Echantillon numéro', echantillon)
+    for echantillon in range(1, nbEchantillon + 1):
+        print('Echantillon numéro', echantillon)
 
-        echantillonDir = os.path.join(dirname,'echantillons', str(echantillon))
+        echantillonDir = os.path.join(
+            dirname, 'echantillons', str(echantillon))
         os.makedirs(echantillonDir)
 
-        # On pioche des mails aléatoires et on les copie dans le dossier échantillon
-        liste_rand = numpy.random.randint(1, mails_count+1,tailleEchantillon)
-        
+        # On pioche des mails aléatoires et on les copie dans le dossier
+        # échantillon
+        liste_rand = numpy.random.randint(
+            1, mails_count + 1, tailleEchantillon)
+
         id_mail = 0
         for repertoire, sousRepertoires, fichiers in os.walk(mailDir):
-            for f in fichiers :
-                if id_mail in liste_rand :
+            for f in fichiers:
+                if id_mail in liste_rand:
                     shutil.copy(os.path.join(repertoire, f), echantillonDir)
-                    os.rename(os.path.join(echantillonDir, f),os.path.join(echantillonDir, str(id_mail)))
-                id_mail +=1
-        print ("Création de l'échantillon", echantillon, "réalisé avec succès :")
-        print (tailleEchantillon, "mails aléatoires ont été copiés dans le répertoire numéro", echantillon, "\n")
-    
-    print ('OK -', nbEchantillon, "échantillon(s) de", tailleEchantillon, 'mails créé(s) dans le(s) répertoire(s) cible(s).\n')
+                    os.rename(
+                        os.path.join(
+                            echantillonDir, f), os.path.join(
+                            echantillonDir, str(id_mail)))
+                id_mail += 1
+        print(
+            "Création de l'échantillon",
+            echantillon,
+            "réalisé avec succès :")
+        print(
+            tailleEchantillon,
+            "mails aléatoires ont été copiés dans le répertoire numéro",
+            echantillon,
+            "\n")
+
+    print('OK -', nbEchantillon, "échantillon(s) de", tailleEchantillon,
+          'mails créé(s) dans le(s) répertoire(s) cible(s).\n')
 
 
 creation_echantillon_mails(3, 10)
